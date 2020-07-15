391f10e9087303cba35693c4f7970b3d7f200fa3
AbelNC5 <abel.nunez@alumnos.uv.cl>
2020-07-15 01:35:57 +0000

    Update

--- src/main.cc
+++ src/main.cc
@@ -6,8 +6,8 @@ uint64_t* arregloSecuencial = nullptr;
 uint64_t* arregloParalelo = nullptr;
 uint64_t sumaArreglo = 0;
 std::vector<std::thread *> threads;
-//std::vector<std::thread *> threadsSuma;
-//std::mutex candado;
+std::vector<std::thread *> threadSuma;
+std::mutex mux;
 
 
 void fillArray(size_t beginIdx, size_t endIdx, size_t arrayType){
@@ -22,6 +22,16 @@ void fillArray(size_t beginIdx, size_t endIdx, size_t arrayType){
 		}
 	}
 }
+
+
+void sumaParalela(uint32_t beginIdx, uint32_t endIdx){
+	mux.lock();
+	for(uint32_t i = beginIdx; i < endIdx; i++){
+		sumaArreglo += arregloParalelo[i];
+	}
+	mux.unlock();
+}
+
 
 
 int main(int argc, char** argv){
@@ -45,8 +55,10 @@ int main(int argc, char** argv){
 
 
 	//Etapa de llenado
+
+	//En esta sección se utilizo una de las funciones thread-safe del ejemplo, para generar números randomicos 
+	//que se almacenaran en un arreglo auxiliar
 	arreglo = new uint64_t[totalElementos];
-	//std::srand(std::time(0));
 	std::random_device device;
 	std::mt19937 rng(device());
 	std::uniform_int_distribution<> unif(limInferior,limSuperior);
@@ -69,11 +81,13 @@ int main(int argc, char** argv){
 	//-------Paralelo-------
 	arregloParalelo = new uint64_t[totalElementos];
 
+	start = std::chrono::high_resolution_clock::now();
+
+	//se crean los threads y se distribuye el contenido del primer arreglo auxiliar
+	//gracias a estos el proceso se hace en forma paralela
 	for(size_t i=0; i < numThreads; i++){
 		threads.push_back(new std::thread(fillArray, i*(totalElementos)/numThreads, (i+1)*(totalElementos)/numThreads, 1));
 	}
-
-	start = std::chrono::high_resolution_clock::now();
 
 	for(auto &thFilled : threads){
 		thFilled->join();
@@ -83,24 +97,44 @@ int main(int argc, char** argv){
 	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end-start);
 	auto tiempoLlenadoTotal_P = elapsed.count();
 
-	std::cout << std::endl;
-
 	//Etapa de suma
 
 	//Secuencial
+	uint64_t sumaSecuencial=0;
+	start = std::chrono::high_resolution_clock::now();
+	for(size_t i=0;i<totalElementos;i++){
+		sumaSecuencial += arregloSecuencial[i];
+	}
+	end = std::chrono::high_resolution_clock::now();
+	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	auto tiempoTotalSuma_S = elapsed.count();
 
-	//Paralelo
+	//Paralela
+	start = std::chrono::high_resolution_clock::now();
+	//De forma similar al llenado 
+	for(size_t i=0;i<numThreads;i++){
+		threadSuma.push_back(new std::thread(sumaParalela, i*(totalElementos)/numThreads, (i+1)*(totalElementos)/numThreads));
+	}
+	for(auto &thSuma : threadSuma){
+		thSuma->join();
+	}
+	end     = std::chrono::high_resolution_clock::now();
+	elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	auto tiempoTotalSuma_P = elapsed.count();
 
-	//Etapa de Resulatados
-	std::cout << "Suma total en paralelo: " << sumaArreglo << std::endl;
+	//Etapa de Resultados
+
+	std::cout << "Suma secuencial total: " << sumaSecuencial << std::endl;
+	std::cout << "Suma en paralelo total: " << sumaArreglo << std::endl;
 	std::cout << "------- Tiempos de ejecución --------" << std::endl;
 	std::cout << "Tiempo Llenado Secuencial: " << tiempoLlenadoTotal_S << "[ms]" << std::endl;
 	std::cout << "Tiempo Llenado Paralelo: " << tiempoLlenadoTotal_P << "[ms]" << std::endl;
 	std::cout << "SpeedUp Etapa de Llenado: " << (double)tiempoLlenadoTotal_S/tiempoLlenadoTotal_P << std::endl;
 	std::cout << "-------------------------------------" << std::endl;
-	//std::cout << "Tiempo Suma Secuencial: " << tiempoTotalSuma_S << "[ms]" << std::endl;
-	//std::cout << "Tiempo Suma Paralela: " << tiempoTotalSuma_P << "[ms]" << std::endl;
-	//std::cout << "SpeedUp Etapa de Suma: " << (double)tiempoTotalSuma_S/tiempoTotalSuma_P << std::endl;
+	std::cout << "Tiempo Suma Secuencial: " << tiempoTotalSuma_S << "[ms]" << std::endl;
+	std::cout << "Tiempo Suma Paralela: " << tiempoTotalSuma_P << "[ms]" << std::endl;
+	std::cout << "SpeedUp Etapa de Suma: " << (double)tiempoTotalSuma_S/tiempoTotalSuma_P << std::endl;
+	std::cout << "-------------------------------------" << std::endl;
 
 	return(EXIT_SUCCESS);
 }
